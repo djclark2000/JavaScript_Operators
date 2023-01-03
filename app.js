de28e7f7e74f9ecc4330eb544722c91767acf680
Dustin Clark <dustinjamesclarkmusic@gmail.com>
2023-01-03 14:11:25 -0600

    completed

--- app.js
+++ app.js
@@ -7,13 +7,58 @@ console.log(
 console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
+let a = 20; 
+let b = 4; 
+let minus = a - b; 
+let multiply = a * b; 
+let dividing = a / b; 
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
+num + str;
+'1111';  
+
+num + str2; 
+'11eleven'; 
+
+num + isPresent; 
+12; 
+
+firstName + num; 
+'Frodo11'; 
+
+isPresent + str; 
+'true11'; 
+
+firstName + lastName; 
+'FrodoBaggins'; 
+
+//
+
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+val == str3; 
+true; 
+
+val === str3;
+false; 
+
+!isAwake; 
+true; 
+
+0 == false; 
+true; 
+
+0 === false; 
+false; 
+
+0 != false; 
+false; 
+
+0 !== false; 
+true; 
